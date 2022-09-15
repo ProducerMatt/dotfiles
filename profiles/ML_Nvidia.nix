b5d3b31d0fe6e7dec64ce664d28dd8f2f0f5f518
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-09-14 19:16:59 -0500

    podman nvidia

--- profiles/ML_Nvidia.nix
+++ profiles/ML_Nvidia.nix
@@ -1,11 +1,12 @@
 {
-  pkgs.config = {
+  nixpkgs.config = {
     allowUnfree = true;
     cudaSupport = true;
   };
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.enable = true;
 
+  virtualisation.podman.enableNvidia = true;
   # Optionally, you may need to select the appropriate driver version for your specific GPU.
   #hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
 }
