a7cf8ee56a0266fff64817ae644a3fdf64b66f58
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-09-14 21:06:31 -0500

    switch podman for docker

--- profiles/ML_Nvidia.nix
+++ profiles/ML_Nvidia.nix
@@ -6,7 +6,7 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.enable = true;
 
-  virtualisation.podman.enableNvidia = true;
+  virtualisation.docker.enableNvidia = true;
   # Optionally, you may need to select the appropriate driver version for your specific GPU.
   #hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
 }
--- profiles/containers.nix
+++ profiles/containers.nix
@@ -1,12 +1,12 @@
 { ... }:
 
 {
-  virtualisation = {
-    podman = {
-      enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-    };
-  };
+  virtualisation.podman.enable = true;
+  #virtualisation = {
+  #  podman = {
+  #    enable = true;
+  #    # Create a `docker` alias for podman, to use it as a drop-in replacement
+  #    dockerCompat = true;
+  #  };
+  #};
 }
