4d49bb02329ce18c3c4c9b22c2db6d6303f09450
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-09-15 20:30:40 -0500

    correct nvidia stuff

--- profiles/ML_Nvidia.nix
+++ profiles/ML_Nvidia.nix
@@ -1,10 +1,17 @@
+{ pkgs, ... }:
 {
   nixpkgs.config = {
     allowUnfree = true;
     cudaSupport = true;
   };
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.opengl.enable = true;
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+    #extraPackages = [ pkgs.nvidia_gl ];
+    #extraPackages32 = [ pkgs.nvidia_gl_32 ];
+  };
 
   virtualisation.docker.enableNvidia = true;
   # Optionally, you may need to select the appropriate driver version for your specific GPU.
