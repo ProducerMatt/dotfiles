2e7642bd54d794a20ecc79e34e573fa9263f7ba4
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-09-18 00:07:12 -0500

    add iosevka

--- profiles/fonts.nix
+++ profiles/fonts.nix
@@ -1,20 +1,42 @@
 { pkgs, ... }:
 {
-    fonts = {
-        enableDefaultFonts = true;
-        fonts = with pkgs; [
-            cantarell-fonts
-            emacs-all-the-icons-fonts
-            font-awesome
-            hasklig
-            iosevka
-            source-code-pro
-            texlive.combined.scheme-full
-            nerdfonts
-        ];
-        fontDir.enable = true;
-        fontconfig = {
-            enable = true;
-        };
+  fonts = {
+    enableDefaultFonts = true;
+    fonts = with pkgs; [
+      cantarell-fonts
+      emacs-all-the-icons-fonts
+      font-awesome
+      hasklig
+      iosevka
+      source-code-pro
+      texlive.combined.scheme-full
+      nerdfonts
+      (iosevka.override {
+        privateBuildPlan = ''
+          [buildPlans.iosevka-matt]
+          family = "Iosevka Matt"
+          spacing = "normal"
+          serifs = "sans"
+          no-cv-ss = false
+          export-glyph-names = false
+
+            [buildPlans.iosevka-matt.variants]
+            inherits = "ss08"
+
+            [buildPlans.iosevka-matt.ligations]
+            inherits = "dlig"
+
+          [buildPlans.iosevka-matt.widths.normal]
+          shape = 600
+          menu = 5
+          css = "normal"
+        '';
+        set = "matt";
+      })
+    ];
+    fontDir.enable = true;
+    fontconfig = {
+      enable = true;
     };
+  };
 }
