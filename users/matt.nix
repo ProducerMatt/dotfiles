a053acf31b86f53e07538eff5768e1376b686304
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-10-21 12:30:20 -0500

    add backuppc key

--- users/matt.nix
+++ users/matt.nix
@@ -8,6 +8,7 @@
     extraGroups = [ "networkmanager" "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfK6c9SiwYYRxy10EMVh1sctDgy6JN/fMyzsO1hACnN ProducerMatt login key"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM0MTVh6Bi82YLKJlpo+4fQRQ3mhZWXFD7VcZEPPWHWA backup@BackupPC"
     ];
     shell = pkgs.fish;
   };
