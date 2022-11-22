6009f13fe440c9973911df67e3cb6479bf3a0e2f
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-11-22 01:44:47 -0600

    oops

--- profiles/homeNetwork.nix
+++ profiles/homeNetwork.nix
@@ -4,6 +4,10 @@
   fileSystems."/mnt/PublicNAS" = {
     device = "192.168.1.3:/Public";
     fsType = "nfs";
+    options = [
+      "nfsvers=3"
+      "noatime"
+    ];
   };
   networking.nameservers = [ "192.168.1.61" "192.168.1.16" ]; # TODO change depending on network
 }
