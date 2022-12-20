19d01ba589033a40b7c01035426b17810dc26ec4
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-12-19 23:19:40 -0600

    syncthing: stop this nonsense

--- profiles/mysyncthing.nix
+++ profiles/mysyncthing.nix
@@ -10,22 +10,8 @@
       group = "users";
       dataDir = "/home/matt";
       configDir = "/home/matt/.config/syncthing";
-      overrideDevices = true; # overrides any devices added or deleted through the WebUI
-      overrideFolders = true; # overrides any folders added or deleted through the WebUI
-      devices = {
-        # Is this considered a secret?
-        "Matt-Win10" = {
-          id = "IK73VQ4-G4R4JUP-UGHWAVA-L2FEBIZ-LISJOFH-QECOXUI-TZYQ5GB-GXK67AD";
-          addresses = [ "192.168.1.2:51820" ];
-        };
-      };
-      folders = {
-        "iak23-enrgo" = {
-          # Name of folder in Syncthing, also the folder ID
-          path = "/home/matt/AIModels"; # Which folder to add to Syncthing
-          devices = [ "Matt-Win10" ]; # Which devices to share the folder with
-        };
-      };
+      overrideDevices = false; # overrides any devices added or deleted through the WebUI
+      overrideFolders = false; # overrides any folders added or deleted through the WebUI
       guiAddress = "0.0.0.0:8384";
     };
   };
