efe76a9180478d7f6646b300c4fc9d2be507c567
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-02-13 20:57:05 -0600

    working DisplayLink

--- profiles/touchscreen.nix
+++ profiles/touchscreen.nix
@@ -4,7 +4,13 @@
   boot.extraModulePackages = with config.boot.kernelPackages; [
     evdi
   ];
-  services.xserver.videoDrivers = [ "modesetting" "displaylink" ];
+  services.xserver.videoDrivers = [
+    "modesetting"
+    "displaylink"
+    "evdi"
+  ];
+  services.xserver.displayManager.setupCommands =
+    "${pkgs.displaylink}/bin/DisplayLinkManager &";
   environment.systemPackages = with pkgs; [
     displaylink
   ];
