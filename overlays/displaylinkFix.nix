1d0b854a7d8591d8f16116ea6bae0de34b259a7d
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-03-07 15:36:36 -0600

    update evdi

--- overlays/displaylinkFix.nix
+++ overlays/displaylinkFix.nix
@@ -4,12 +4,12 @@ final: prev: {
       (lpfinal: lpprev: {
         evdi =
           lpprev.evdi.overrideAttrs (efinal: eprev: {
-            version = "1.12.0-git";
+            version = "unstable-2022-02-23";
             src = prev.fetchFromGitHub {
               owner = "DisplayLink";
               repo = "evdi";
-              rev = "bdc258b25df4d00f222fde0e3c5003bf88ef17b5";
-              sha256 = "sha256-mt+vEp9FFf7smmE2PzuH/3EYl7h89RBN1zTVvv2qJ/o=";
+              rev = "64559211ec33390dbf4372e1029455ff58e0e739";
+              sha256 = "0000000000000000000000000000000000000000000000000000";
             };
           });
       });
