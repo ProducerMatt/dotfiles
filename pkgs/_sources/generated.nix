f73efd22606036e7453b12c271f8ebffa7442ba8
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-03-18 22:51:11 -0500

    just get manix from nixpkgs

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -37,18 +37,6 @@
     });
     date = "2022-04-08";
   };
-  manix = {
-    pname = "manix";
-    version = "d08e7ca185445b929f097f8bfb1243a8ef3e10e4";
-    src = fetchFromGitHub ({
-      owner = "mlvzk";
-      repo = "manix";
-      rev = "d08e7ca185445b929f097f8bfb1243a8ef3e10e4";
-      fetchSubmodules = false;
-      sha256 = "sha256-GqPuYscLhkR5E2HnSFV4R48hCWvtM3C++3zlJhiK/aw=";
-    });
-    date = "2021-04-20";
-  };
   nvmfish = {
     pname = "nvmfish";
     version = "9db8eaf6e3064a962bca398edd42162f65058ae8";
