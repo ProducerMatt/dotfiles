9f5e48f8e0eebfd564df383fb259877ea0f24d33
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-05-29 16:52:55 -0500

    re-enable home-manager manpages

--- users/profiles/core.nix
+++ users/profiles/core.nix
@@ -4,6 +4,6 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  # BUG: https://github.com/NixOS/nixpkgs/issues/196651
-  manual.manpages.enable = false;
+  ## BUG: https://github.com/NixOS/nixpkgs/issues/196651
+  #manual.manpages.enable = false;
 }
