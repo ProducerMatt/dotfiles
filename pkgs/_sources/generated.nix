a5d3f5cc93f7d5541001379b86c3e30dc053084e
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-06-17 01:27:19 -0500

    update nvfetcher

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -4,73 +4,73 @@
   fish-abbreviation-tips = {
     pname = "fish-abbreviation-tips";
     version = "8ed76a62bb044ba4ad8e3e6832640178880df485";
-    src = fetchFromGitHub ({
+    src = fetchFromGitHub {
       owner = "gazorby";
       repo = "fish-abbreviation-tips";
       rev = "8ed76a62bb044ba4ad8e3e6832640178880df485";
       fetchSubmodules = false;
       sha256 = "sha256-F1t81VliD+v6WEWqj1c1ehFBXzqLyumx5vV46s/FZRU=";
-    });
+    };
     date = "2023-01-23";
   };
   fzf = {
     pname = "fzf";
-    version = "63c8f8e65761295da51029c5b6c9e601571837a1";
-    src = fetchFromGitHub ({
+    version = "c5e170730b4f8395e116d7c06883ed53da2d5561";
+    src = fetchFromGitHub {
       owner = "PatrickF1";
       repo = "fzf.fish";
-      rev = "63c8f8e65761295da51029c5b6c9e601571837a1";
+      rev = "c5e170730b4f8395e116d7c06883ed53da2d5561";
       fetchSubmodules = false;
-      sha256 = "sha256-i9FcuQdmNlJnMWQp7myF3N0tMD/2I0CaMs/PlD8o1gw=";
-    });
-    date = "2023-01-25";
+      sha256 = "sha256-xWaMd5POCDeeFTsGtHbIvsPelIp+GZPC1X1CseCo3BA=";
+    };
+    date = "2023-05-23";
   };
   jethrokuan-z = {
     pname = "jethrokuan-z";
     version = "85f863f20f24faf675827fb00f3a4e15c7838d76";
-    src = fetchFromGitHub ({
+    src = fetchFromGitHub {
       owner = "jethrokuan";
       repo = "z";
       rev = "85f863f20f24faf675827fb00f3a4e15c7838d76";
       fetchSubmodules = false;
       sha256 = "sha256-+FUBM7CodtZrYKqU542fQD+ZDGrd2438trKM0tIESs0=";
-    });
+    };
     date = "2022-04-08";
   };
   nvmfish = {
     pname = "nvmfish";
     version = "c69e5d1017b21bcfca8f42c93c7e89fff6141a8a";
-    src = fetchFromGitHub ({
+    src = fetchFromGitHub {
       owner = "jorgebucaran";
       repo = "nvm.fish";
       rev = "c69e5d1017b21bcfca8f42c93c7e89fff6141a8a";
       fetchSubmodules = false;
       sha256 = "sha256-LV5NiHfg4JOrcjW7hAasUSukT43UBNXGPi1oZWPbnCA=";
-    });
+    };
     date = "2023-04-30";
   };
   replayfish = {
     pname = "replayfish";
     version = "bd8e5b89ec78313538e747f0292fcaf631e87bd2";
-    src = fetchFromGitHub ({
+    src = fetchFromGitHub {
       owner = "jorgebucaran";
       repo = "replay.fish";
       rev = "bd8e5b89ec78313538e747f0292fcaf631e87bd2";
       fetchSubmodules = false;
       sha256 = "sha256-bM6+oAd/HXaVgpJMut8bwqO54Le33hwO9qet9paK1kY=";
-    });
+    };
     date = "2022-06-19";
   };
   rsync-bpc = {
     pname = "rsync-bpc";
     version = "7215af4f16b729098193d7dde5a8c3c8c4ae80de";
-    src = fetchFromGitHub ({
+    src = fetchFromGitHub {
       owner = "backuppc";
       repo = "rsync-bpc";
       rev = "7215af4f16b729098193d7dde5a8c3c8c4ae80de";
       fetchSubmodules = false;
       sha256 = "sha256-CXGeSWZF0OOhwo+yndkd0ppYILHELBiZIDhHN7BqVYc=";
-    });
+    };
     date = "2021-07-24";
   };
 }
