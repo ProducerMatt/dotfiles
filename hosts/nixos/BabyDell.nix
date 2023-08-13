9380eb0c26cd0a03de2a7a9b47513fec0a9aa74a
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-08-12 22:30:04 -0500

    builder jobs

--- hosts/nixos/BabyDell.nix
+++ hosts/nixos/BabyDell.nix
@@ -21,6 +21,7 @@
         protocol = "ssh-ng";
         sshUser = "nixremote";
         sshKey = "/home/matt/.ssh/id_nixremote_ed25519";
+        maxJobs = 16;
         speedFactor = 8;
         supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
       }
