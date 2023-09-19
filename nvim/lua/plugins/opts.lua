0539fa0064b17b760f23e9e7992713896c266443
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-09-19 14:19:50 -0500

    nvim: tweaks

---
+++ nvim/lua/plugins/opts.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      autoformat = false
+    },
+  },
+  {
+    "gbprod/yanky.nvim",
+    opts = {
+      ring = {
+        storage = "memory",
+      },
+    },
+  },
+}
