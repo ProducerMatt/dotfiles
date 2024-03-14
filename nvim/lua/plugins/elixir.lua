b158adaf8e8907b17eeb32852f1ce29518bf67a4
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2024-03-14 16:40:28 -0500

    nvim: switch to nextls

--- nvim/lua/plugins/elixir.lua
+++ nvim/lua/plugins/elixir.lua
@@ -17,20 +17,23 @@ return {
         local elixirls = require("elixir.elixirls")
 
         elixir.setup {
-          nextls = {enable = false},
-          credo = {enable = true},
-          elixirls = {
+          nextls = {
             enable = true,
-            settings = elixirls.settings {
-              dialyzerEnabled = true,
-              enableTestLenses = true,
-              suggestSpecs = true,
-            },
-            on_attach = function(client, bufnr)
-              vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-              vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-              vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
-            end,
+
+          },
+          credo = {enable = false},
+          elixirls = {
+            enable = false,
+          --  settings = elixirls.settings {
+          --    dialyzerEnabled = true,
+          --    enableTestLenses = true,
+          --    suggestSpecs = true,
+          --  },
+          --  on_attach = function(client, bufnr)
+          --    vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+          --    vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+          --    vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+          --  end,
           }
         }
       end,
