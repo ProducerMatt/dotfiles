5ab9155d03d6ff4f10fadcd74f8736bbbc9f45be
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2024-03-28 18:10:59 -0500

    restore snippets

---
+++ snippets/git.nix
@@ -0,0 +1,8 @@
+pkgs:
+
+with pkgs; [
+  git-crypt
+  lazygit
+  git-ignore
+  nur.repos.ProducerMatt.ripsecrets
+]
---
+++ snippets/sysadmin.nix
@@ -0,0 +1,10 @@
+pkgs:
+
+with pkgs; [
+  ethtool
+  smartmontools
+  #(nur.repos.ProducerMatt.cosmo.override {
+  #    appList = ["pledge"];
+  #    distRepo = false;
+  #  })
+]
