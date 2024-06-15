15469512f92b7b8c2fabb3c3dc92f1b77e7fe529
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2024-06-15 17:14:53 -0500

    re-enable opengl for jellyfin

--- profiles/jellyfin.nix
+++ profiles/jellyfin.nix
@@ -1,17 +1,17 @@
-{
-  #nixpkgs.config.packageOverrides = pkgs: {
-  #  vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  #};
-  #hardware.opengl = {
-  #  enable = true;
-  #  extraPackages = with pkgs; [
-  #    intel-media-driver
-  #    vaapiIntel
-  #    vaapiVdpau
-  #    libvdpau-va-gl
-  #    intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
-  #  ];
-  #};
+{pkgs, ...}: {
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+    ];
+  };
 
   services.jellyfin = {
     enable = true;
