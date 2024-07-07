ff696ba6c7c278ef5b10ebb70c13aed2bdf9605b
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2024-07-06 22:58:39 -0500

    feat(llm): ollama latest version

--- profiles/local_llm.nix
+++ profiles/local_llm.nix
@@ -1,8 +1,20 @@
-{
+{pkgs, ...}: {
   services.ollama = {
     enable = true;
     openFirewall = true;
     host = "0.0.0.0";
     acceleration = false;
+    package = pkgs.ollama.overrideAttrs (final: prev: let
+      version = "0.1.49-rc7";
+    in {
+      inherit version;
+      src = pkgs.fetchFromGitHub {
+        owner = "ollama";
+        repo = "ollama";
+        rev = "v${version}";
+        hash = "sha256-ZIJsveQK1WM6GCLsjcp0E//wtLN80qJ8MM5TyzGtBSY=";
+        fetchSubmodules = true;
+      };
+    });
   };
 }
