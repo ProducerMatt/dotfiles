f84ad9ec4ab8951d70a23679e766b4496a4262b5
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2024-08-07 12:43:01 -0500

    feat(nvidia)!: experiment with nvidia settings

--- profiles/ML_Nvidia.nix
+++ profiles/ML_Nvidia.nix
@@ -1,18 +1,42 @@
 {
+  config,
+  lib,
+  ...
+}: {
   nixpkgs.config = {
     # TODO: whitelist unfree pkgs
     allowUnfree = true;
     cudaSupport = true;
   };
-  services.xserver.videoDrivers = ["nvidia"];
+
+  services.xserver = {
+    enable = true;
+    videoDrivers = ["nvidia"];
+  };
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    #extraPackages = [ pkgs.nvidia_gl ];
-    #extraPackages32 = [ pkgs.nvidia_gl_32 ];
   };
 
   hardware.nvidia-container-toolkit.enable = true;
-  # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  #hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+  # ensure text mode works
+  boot = {
+    initrd.kernelModules = ["nvidia"];
+    kernelParams = [
+      # intel
+      "module_blacklist=i915"
+      # AMD
+      "module_blacklist=amdgpu"
+    ];
+    extraModulePackages = [config.boot.kernelPackages.nvidia_x11];
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    # # INFO: power management for PRIME
+    # powerManagement.enable = true;
+    # powerManagement.finegrained = false;
+    # open = true;
+  };
 }
