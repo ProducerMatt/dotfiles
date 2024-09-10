a17f263c2a8b4f4ae4983cc562caa55b6e7f2d86
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2024-09-10 17:30:27 -0500

    feat(desktop): add plasma desktop

--- hosts/PortableNix/default.nix
+++ hosts/PortableNix/default.nix
@@ -4,16 +4,9 @@
   lib,
   profiles,
   users,
+  modules,
   ...
-}:
-{
-  # age.secrets."wg-PortableNix.key".file =
-  #   ../../secrets/wg-PortableNix.key.age;
-  # age.secrets."wg-apu4VPN-preshared.key".file =
-  #   ../../secrets/wg-apu4VPN-preshared.key.age;
-
-  users.mutableUsers = true;
-
+}: {
   imports = with profiles; [
     core
     cachix
@@ -35,144 +28,153 @@
     #profiles.virtualbox
     #profiles.fonts
     local_llm
+    disable_sleep
+    modules.mattsDesktop
   ];
 
-  nix.settings.system-features = ["nixos-test" "benchmark" "big-parallel" "kvm"];
-  # nixpkgs.localSystem = {
-  #   #  gcc.arch = "skylake";
-  #   #  gcc.tune = "skylake";
-  #   system = "x86_64-linux";
-  # };
+  config =
+    {
+      # age.secrets."wg-PortableNix.key".file =
+      #   ../../secrets/wg-PortableNix.key.age;
+      # age.secrets."wg-apu4VPN-preshared.key".file =
+      #   ../../secrets/wg-apu4VPN-preshared.key.age;
 
-  # services.mattsDesktop = {
-  #   enable = false;
-  #   sound = true;
-  #   printing = true;
-  #   displayLink = false;
-  #   desktop = "gnome";
-  #   autoStart = true;
-  #   autoLogin = true;
-  # };
+      users.mutableUsers = true;
 
-  # services.mattsRemoteAccess = {
-  #   enable = false;
-  # };
+      nix.settings.system-features = ["nixos-test" "benchmark" "big-parallel" "kvm"];
+      # nixpkgs.localSystem = {
+      #   #  gcc.arch = "skylake";
+      #   #  gcc.tune = "skylake";
+      #   system = "x86_64-linux";
+      # };
 
-  # services.mattsNetwork.hostname = "PortableNix";
-
-  networking = {
-    hostName = "PortableNix";
-    interfaces = {
-      "enp112s0" = {
-        useDHCP = false;
-        ipv4.addresses = [
-          {
-            address = "192.168.1.5";
-            prefixLength = 16;
-          }
-        ];
+      services.mattsDesktop = {
+        enable = true;
+        sound = true;
+        printing = false;
+        displayLink = false;
+        desktop = "plasma";
+        autoStart = false;
+        autoLogin = true;
+        remote.enable = true;
       };
-    };
-    hosts = {
-      "192.168.1.3" = ["PherigoNAS.local"];
-    };
-  };
 
-  fileSystems = {
-    "/mnt/PublicNAS" = {
-      device = "PherigoNAS.local:/mnt/PherigoRAID/Public";
-      fsType = "nfs";
-      options = [
-        "nfsvers=4"
-        "noatime"
-        "noexec"
-      ];
-    };
-    "/mnt/MattNAS" = {
-      device = "PherigoNAS.local:/mnt/PherigoRAID/Matt";
-      fsType = "nfs";
-      options = [
-        "nfsvers=4"
-        "noatime"
-        "noexec"
-      ];
-    };
-  };
+      # services.mattsNetwork.hostname = "PortableNix";
 
-  # programs.apeLoader = {
-  #   enable = false;
-  #   mode = "workaround";
-  # };
+      networking = {
+        hostName = "PortableNix";
+        interfaces = {
+          "enp112s0" = {
+            useDHCP = false;
+            ipv4.addresses = [
+              {
+                address = "192.168.1.5";
+                prefixLength = 16;
+              }
+            ];
+          };
+        };
+        hosts = {
+          "192.168.1.3" = ["PherigoNAS.local"];
+        };
+      };
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "uas" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-intel"];
-  boot.extraModulePackages = [];
+      fileSystems = {
+        "/mnt/PublicNAS" = {
+          device = "PherigoNAS.local:/mnt/PherigoRAID/Public";
+          fsType = "nfs";
+          options = [
+            "nfsvers=4"
+            "noatime"
+            "noexec"
+          ];
+        };
+        "/mnt/MattNAS" = {
+          device = "PherigoNAS.local:/mnt/PherigoRAID/Matt";
+          fsType = "nfs";
+          options = [
+            "nfsvers=4"
+            "noatime"
+            "noexec"
+          ];
+        };
+      };
 
-  services.xserver.videoDrivers = ["nvidia"];
+      # programs.apeLoader = {
+      #   enable = false;
+      #   mode = "workaround";
+      # };
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/4a5f9903-bca6-46e6-ae33-56494b15016c";
-    fsType = "ext4";
-  };
+      boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "uas" "usb_storage" "sd_mod"];
+      boot.initrd.kernelModules = [];
+      boot.kernelModules = ["kvm-intel"];
+      boot.extraModulePackages = [];
 
-  boot.initrd.luks.devices."luks-c05dea80-c718-46cf-85b8-9f3c6aeca86c".device = "/dev/disk/by-uuid/c05dea80-c718-46cf-85b8-9f3c6aeca86c";
+      services.xserver.videoDrivers = ["nvidia"];
 
-  swapDevices = [{device = "/dev/disk/by-uuid/bef52767-f7fc-405f-9423-d7b576e79c86";}];
+      fileSystems."/" = {
+        device = "/dev/disk/by-uuid/4a5f9903-bca6-46e6-ae33-56494b15016c";
+        fsType = "ext4";
+      };
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
+      boot.initrd.luks.devices."luks-c05dea80-c718-46cf-85b8-9f3c6aeca86c".device = "/dev/disk/by-uuid/c05dea80-c718-46cf-85b8-9f3c6aeca86c";
 
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+      swapDevices = [{device = "/dev/disk/by-uuid/bef52767-f7fc-405f-9423-d7b576e79c86";}];
 
-  # Latest kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+      # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+      # (the default) this is the recommended approach. When using systemd-networkd it's
+      # still possible to use this option, but it's recommended to use it in conjunction
+      # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+      # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
 
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+      hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  # Setup keyfile
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
-  };
+      # Latest kernel
+      boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # Enable grub cryptodisk
-  boot.loader.grub.enableCryptodisk = true;
+      # Bootloader.
+      boot.loader.grub.enable = true;
+      boot.loader.grub.device = "/dev/sda";
+      boot.loader.grub.useOSProber = true;
 
-  boot.initrd.luks.devices."luks-c05dea80-c718-46cf-85b8-9f3c6aeca86c".keyFile = "/crypto_keyfile.bin";
-  # Enable swap on luks
-  boot.initrd.luks.devices."luks-90dc3598-b566-4723-8c36-ba6110d2bd98".device = "/dev/disk/by-uuid/90dc3598-b566-4723-8c36-ba6110d2bd98";
-  boot.initrd.luks.devices."luks-90dc3598-b566-4723-8c36-ba6110d2bd98".keyFile = "/crypto_keyfile.bin";
+      # Setup keyfile
+      boot.initrd.secrets = {
+        "/crypto_keyfile.bin" = null;
+      };
 
-  # networking = {
-  #   wireguard.interfaces =
-  #     import ../../secrets/wg-PortableNix.nix {
-  #       privateKeyPath =
-  #         config.age.secrets."wg-PortableNix.key".path;
-  #       apuKeyPath =
-  #         config.age.secrets."wg-apu4VPN-preshared.key".path;
-  #     };
-  # };
+      # Enable grub cryptodisk
+      boot.loader.grub.enableCryptodisk = true;
 
-  # Set your time zone.
-  time.timeZone = "America/Chicago";
+      boot.initrd.luks.devices."luks-c05dea80-c718-46cf-85b8-9f3c6aeca86c".keyFile = "/crypto_keyfile.bin";
+      # Enable swap on luks
+      boot.initrd.luks.devices."luks-90dc3598-b566-4723-8c36-ba6110d2bd98".device = "/dev/disk/by-uuid/90dc3598-b566-4723-8c36-ba6110d2bd98";
+      boot.initrd.luks.devices."luks-90dc3598-b566-4723-8c36-ba6110d2bd98".keyFile = "/crypto_keyfile.bin";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.utf8";
+      # networking = {
+      #   wireguard.interfaces =
+      #     import ../../secrets/wg-PortableNix.nix {
+      #       privateKeyPath =
+      #         config.age.secrets."wg-PortableNix.key".path;
+      #       apuKeyPath =
+      #         config.age.secrets."wg-apu4VPN-preshared.key".path;
+      #     };
+      # };
 
-  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
+      # Set your time zone.
+      time.timeZone = "America/Chicago";
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
+      # Select internationalisation properties.
+      i18n.defaultLocale = "en_US.utf8";
+
+      powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
+
+      # This value determines the NixOS release from which the default
+      # settings for stateful data, like file locations and database versions
+      # on your system were taken. It‘s perfectly fine and recommended to leave
+      # this value at the release version of the first install of this system.
+      # Before changing this value read the documentation for this option
+      # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+      system.stateVersion = "22.05"; # Did you read the comment?
+    }
+    // overrides;
 }
-// overrides
---
+++ modules/mattsDesktop/default.nix
@@ -0,0 +1,247 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.services.mattsDesktop;
+in {
+  options.services.mattsDesktop = with lib; {
+    enable = mkEnableOption "Use Matt's Desktop settings";
+    sound = mkEnableOption "Enable sound systems";
+    printing = mkEnableOption "Enable printing systems";
+    displayLink = mkEnableOption "Enable DisplayLink support";
+    desktop = mkOption {
+      description = "Which type of desktop to set up.";
+      type = with types; nullOr (enum ["plasma" "sway" "gnome" "pantheon"]);
+      default = "plasma";
+    };
+    autoStart = mkEnableOption "Start the GUI on boot";
+    autoLogin = mkEnableOption "Autologin to the Desktop";
+    remote = mkOption {
+      description = "How to handle remote access";
+      default = {enable = false;};
+      type = types.submodule {
+        options = {
+          enable = mkEnableOption "Enable remote desktop server";
+          type = mkOption {
+            description = "Which type of remote desktop service to use. Currently only RDP";
+            type = with types; strMatching "RDP";
+            default = "RDP";
+          };
+          port = mkOption {
+            description = "Port to listen on.";
+            type = with types; uniq port;
+            default = 3389;
+          };
+        };
+      };
+    };
+  };
+  config = with lib;
+    mkIf cfg.enable (mkMerge [
+      {
+        environment.systemPackages = with pkgs; [
+          kitty
+        ];
+      }
+      (mkIf (cfg.desktop == "plasma") {
+        # Enable the X11 windowing system.
+        services.xserver = {
+          enable = true;
+          autorun = cfg.autoStart;
+          displayManager.autoLogin =
+            if cfg.autoLogin
+            then {
+              enable = true;
+              user = "matt";
+            }
+            else {enable = false;};
+        };
+
+        # Enable the KDE Plasma Desktop Environment.
+        services.xserver.displayManager.sddm.enable = true;
+        services.xserver.desktopManager.plasma5.enable = true;
+
+        # Configure keymap in X11
+        services.xserver = {
+          layout = "us";
+          xkbVariant = "";
+        };
+
+        environment.systemPackages = with pkgs; [
+          kdeplasma-addons
+
+          # graphics debug tools
+          xorg.xdpyinfo
+          glxinfo
+          vulkan-tools
+          libsForQt5.qt5.qttools.bin # qdbus
+        ];
+        # Enable touchpad support (enabled default in most desktopManager).
+        services.xserver.libinput.enable = true;
+      })
+      (mkIf ((cfg.desktop == "plasma") && cfg.displayLink) {
+        boot.extraModulePackages = with config.boot.kernelPackages; [
+          evdi
+        ];
+        services.xserver.videoDrivers = [
+          "modesetting"
+          "displaylink"
+          "evdi"
+        ];
+        services.xserver.displayManager.setupCommands = "${pkgs.displaylink}/bin/DisplayLinkManager &";
+        environment.systemPackages = with pkgs; [
+          displaylink
+        ];
+      })
+      (mkIf
+        ((cfg.desktop == "plasma") && cfg.remote.enable)
+        {
+          services.xrdp = {
+            enable = true;
+            openFirewall = true;
+            port = cfg.remote.port;
+            defaultWindowManager = "${pkgs.libsForQt5.plasma-workspace.out}/bin/startplasma-x11";
+          };
+        })
+      (mkIf (cfg.desktop == "gnome") {
+        # Enable the X11 windowing system.
+        services.xserver = {
+          enable = true;
+          displayManager.gdm.enable = true;
+          desktopManager.gnome.enable = true;
+          autorun = cfg.autoStart;
+          displayManager.autoLogin =
+            if cfg.autoLogin
+            then {
+              enable = true;
+              user = "matt";
+            }
+            else {enable = false;};
+        };
+        environment.gnome.excludePackages = with pkgs; [
+          firefox
+        ];
+        # Configure keymap in X11
+        services.xserver = {
+          layout = "us";
+          xkbVariant = "";
+        };
+
+        environment.systemPackages = with pkgs; [
+          # graphics debug tools
+          xorg.xdpyinfo
+          glxinfo
+          vulkan-tools
+          libsForQt5.qt5.qttools.bin # qdbus
+        ];
+        # Enable touchpad support (enabled default in most desktopManager).
+        services.xserver.libinput.enable = true;
+      })
+      (mkIf ((cfg.desktop == "gnome") && cfg.displayLink) {
+        boot.extraModulePackages = with config.boot.kernelPackages; [
+          evdi
+        ];
+        services.xserver.videoDrivers = [
+          "modesetting"
+          "displaylink"
+          "evdi"
+        ];
+        services.xserver.displayManager.setupCommands = "${pkgs.displaylink}/bin/DisplayLinkManager &";
+        environment.systemPackages = with pkgs; [
+          displaylink
+        ];
+      })
+      (mkIf (cfg.desktop == "sway") {
+        programs.sway = {
+          enable = true;
+          wrapperFeatures.gtk = true;
+        };
+      })
+      (mkIf ((cfg.desktop == "sway") && cfg.displayLink) {
+        boot.extraModulePackages = with config.boot.kernelPackages; [
+          evdi
+        ];
+        #services.xserver.videoDrivers = [
+        #  "modesetting"
+        #  "displaylink"
+        #  "evdi"
+        #];
+        programs.sway.extraSessionCommands = ''
+          ${pkgs.displaylink}/bin/DisplayLinkManager &
+        '';
+        environment.systemPackages = with pkgs; [
+          displaylink
+        ];
+      })
+
+      (mkIf (cfg.desktop == "pantheon") {
+        # Enable the X11 windowing system.
+        services.xserver = {
+          enable = true;
+          displayManager.lightdm.enable = true;
+          desktopManager.pantheon.enable = true;
+          autorun = cfg.autoStart;
+          displayManager.autoLogin =
+            if cfg.autoLogin
+            then {
+              enable = true;
+              user = "matt";
+            }
+            else {enable = false;};
+        };
+        # Configure keymap in X11
+        services.xserver = {
+          layout = "us";
+          xkbVariant = "";
+        };
+
+        environment.systemPackages = with pkgs; [
+          # graphics debug tools
+          xorg.xdpyinfo
+          glxinfo
+          vulkan-tools
+          libsForQt5.qt5.qttools.bin # qdbus
+        ];
+        # Enable touchpad support (enabled default in most desktopManager).
+        services.xserver.libinput.enable = true;
+      })
+      (mkIf ((cfg.desktop == "pantheon") && cfg.displayLink) {
+        boot.extraModulePackages = with config.boot.kernelPackages; [
+          evdi
+        ];
+        services.xserver.videoDrivers = [
+          "modesetting"
+          "displaylink"
+          "evdi"
+        ];
+        services.xserver.displayManager.setupCommands = "${pkgs.displaylink}/bin/DisplayLinkManager &";
+        environment.systemPackages = with pkgs; [
+          displaylink
+        ];
+      })
+
+      (mkIf cfg.sound {
+        sound.enable = true;
+        hardware.pulseaudio.enable = false;
+        security.rtkit.enable = true;
+        services.pipewire = {
+          enable = true;
+          alsa.enable = true;
+          alsa.support32Bit = true;
+          pulse.enable = true;
+          # If you want to use JACK applications, uncomment this
+          #jack.enable = true;
+
+          # use the example session manager (no others are packaged yet so this is enabled by default,
+          # no need to redefine it in your config for now)
+          #media-session.enable = true;
+        };
+      })
+      (mkIf cfg.printing {
+        # Enable CUPS to print documents.
+        services.printing.enable = true;
+      })
+    ]);
+}
