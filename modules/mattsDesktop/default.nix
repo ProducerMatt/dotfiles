bc81bf58da6b08806fe8ec12776063971014fd3c
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2025-01-31 18:53:20 -0600

    feat(mattsDesktop): shred duplicate code

--- modules/mattsDesktop/default.nix
+++ modules/mattsDesktop/default.nix
@@ -11,11 +11,6 @@ in {
     sound = mkEnableOption "Enable sound systems";
     printing = mkEnableOption "Enable printing systems";
     displayLink = mkEnableOption "Enable DisplayLink support";
-    desktop = mkOption {
-      description = "Which type of desktop to set up.";
-      type = with types; nullOr (enum ["plasma" "sway" "gnome" "pantheon"]);
-      default = "plasma";
-    };
     autoStart = mkEnableOption "Start the GUI on boot";
     autoLogin = mkEnableOption "Autologin to the Desktop";
     remote = {
@@ -35,11 +30,6 @@ in {
   config = with lib;
     mkIf cfg.enable (mkMerge [
       {
-        environment.systemPackages = with pkgs; [
-          kitty
-        ];
-      }
-      (mkIf (cfg.desktop == "plasma") {
         # Enable the X11 windowing system.
         services.xserver = {
           enable = true;
@@ -73,11 +63,14 @@ in {
           glxinfo
           vulkan-tools
           libsForQt5.qt5.qttools.bin # qdbus
+
+          firefox
+          kitty
         ];
         # Enable touchpad support (enabled default in most desktopManager).
         services.libinput.enable = true;
-      })
-      (mkIf ((cfg.desktop == "plasma") && cfg.displayLink) {
+      }
+      (mkIf cfg.displayLink {
         boot.extraModulePackages = with config.boot.kernelPackages; [
           evdi
         ];
@@ -92,8 +85,7 @@ in {
         ];
       })
       (
-        mkIf
-        ((cfg.desktop == "plasma") && cfg.remote.enable)
+        mkIf cfg.remote.enable
         (mkMerge [
           (mkIf (cfg.remote.type == "RDP")
             {
@@ -112,123 +104,6 @@ in {
             })
         ])
       )
-      (mkIf (cfg.desktop == "gnome") {
-        # Enable the X11 windowing system.
-        services.xserver = {
-          enable = true;
-          displayManager.gdm.enable = true;
-          displayManager.defaultSession = "";
-          desktopManager.gnome.enable = true;
-          autorun = mkForce cfg.autoStart;
-          displayManager.autoLogin =
-            if cfg.autoLogin
-            then {
-              enable = true;
-              user = "matt";
-            }
-            else {enable = false;};
-        };
-        environment.gnome.excludePackages = with pkgs; [
-          firefox
-        ];
-        # Configure keymap in X11
-        services.xserver = {
-          xkb.layout = "us";
-          xkb.variant = "";
-        };
-
-        environment.systemPackages = with pkgs; [
-          # graphics debug tools
-          xorg.xdpyinfo
-          glxinfo
-          vulkan-tools
-          libsForQt5.qt5.qttools.bin # qdbus
-        ];
-        # Enable touchpad support (enabled default in most desktopManager).
-        services.libinput.enable = true;
-      })
-      (mkIf ((cfg.desktop == "gnome") && cfg.displayLink) {
-        boot.extraModulePackages = with config.boot.kernelPackages; [
-          evdi
-        ];
-        services.xserver.videoDrivers = [
-          "modesetting"
-          "displaylink"
-          "evdi"
-        ];
-        services.xserver.displayManager.setupCommands = "${pkgs.displaylink}/bin/DisplayLinkManager &";
-        environment.systemPackages = with pkgs; [
-          displaylink
-        ];
-      })
-      (mkIf (cfg.desktop == "sway") {
-        programs.sway = {
-          enable = true;
-          wrapperFeatures.gtk = true;
-        };
-      })
-      (mkIf ((cfg.desktop == "sway") && cfg.displayLink) {
-        boot.extraModulePackages = with config.boot.kernelPackages; [
-          evdi
-        ];
-        #services.xserver.videoDrivers = [
-        #  "modesetting"
-        #  "displaylink"
-        #  "evdi"
-        #];
-        programs.sway.extraSessionCommands = ''
-          ${pkgs.displaylink}/bin/DisplayLinkManager &
-        '';
-        environment.systemPackages = with pkgs; [
-          displaylink
-        ];
-      })
-      (mkIf (cfg.desktop == "pantheon") {
-        # Enable the X11 windowing system.
-        services.xserver = {
-          enable = true;
-          displayManager.lightdm.enable = true;
-          displayManager.defaultSession = "";
-          desktopManager.pantheon.enable = true;
-          autorun = mkForce cfg.autoStart;
-          displayManager.autoLogin =
-            if cfg.autoLogin
-            then {
-              enable = true;
-              user = "matt";
-            }
-            else {enable = false;};
-        };
-        # Configure keymap in X11
-        services.xserver = {
-          xkb.layout = "us";
-          xkb.variant = "";
-        };
-
-        environment.systemPackages = with pkgs; [
-          # graphics debug tools
-          xorg.xdpyinfo
-          glxinfo
-          vulkan-tools
-          libsForQt5.qt5.qttools.bin # qdbus
-        ];
-        # Enable touchpad support (enabled default in most desktopManager).
-        services.libinput.enable = true;
-      })
-      (mkIf ((cfg.desktop == "pantheon") && cfg.displayLink) {
-        boot.extraModulePackages = with config.boot.kernelPackages; [
-          evdi
-        ];
-        services.xserver.videoDrivers = [
-          "modesetting"
-          "displaylink"
-          "evdi"
-        ];
-        services.xserver.displayManager.setupCommands = "${pkgs.displaylink}/bin/DisplayLinkManager &";
-        environment.systemPackages = with pkgs; [
-          displaylink
-        ];
-      })
       (mkIf cfg.sound {
         hardware.pulseaudio.enable = false;
         security.rtkit.enable = true;
