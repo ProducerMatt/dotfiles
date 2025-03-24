7da16cf78a784f213aa125dd8bb30abaa6c44c14
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2025-03-23 19:34:47 -0500

    feat(jellyfin): use legacy intel drivers

--- profiles/jellyfin.nix
+++ profiles/jellyfin.nix
@@ -9,9 +9,16 @@
       vaapiIntel
       vaapiVdpau
       libvdpau-va-gl
-      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      intel-compute-runtime-legacy1 # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      # NOTE: uses legacy package for intel CPUs prior to gen12
+      # for gen12 and up, use intel-compute-runtime
+      # https://discourse.nixos.org/t/trouble-getting-quicksync-to-work-with-jellyfin/42275
+      # https://github.com/NixOS/nixpkgs/issues/356535
+      onevpl-intel-gpu
     ];
   };
+
+  environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";}; # Force intel-media-driver
 
   services.jellyfin = {
     enable = true;
