9afd985599b92747d6beef670bb5a88874163604
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2025-05-27 22:34:55 -0500

    fix: utf-8 capitalization

--- hosts/PortableNix/default.nix
+++ hosts/PortableNix/default.nix
@@ -166,7 +166,7 @@
       time.timeZone = "America/Chicago";
 
       # Select internationalisation properties.
-      i18n.defaultLocale = "en_US.utf8";
+      i18n.defaultLocale = "en_US.UTF-8";
 
       i18n.extraLocaleSettings = {
         LC_ADDRESS = "en_US.UTF-8";
