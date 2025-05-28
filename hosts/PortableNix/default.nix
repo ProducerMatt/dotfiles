6e4d51e50a806af442ff9870cd02e0b310fd6ba8
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2025-05-27 20:38:21 -0500

    feat(PortableNix): restore old hard disk

--- hosts/PortableNix/default.nix
+++ hosts/PortableNix/default.nix
@@ -117,14 +117,13 @@
       services.xserver.videoDrivers = ["nvidia"];
 
       fileSystems."/" = {
-        device = "/dev/disk/by-uuid/acd63d6d-5dc9-405a-82f3-bbc566e6abaa";
-        fsType = "btrfs";
-        options = ["subvol=@"];
+        device = "/dev/disk/by-uuid/4a5f9903-bca6-46e6-ae33-56494b15016c";
+        fsType = "ext4";
       };
 
-      swapDevices = [
-        {device = "/dev/disk/by-uuid/f4d2ab40-c830-465c-9cd9-2eb026269cf5";}
-      ];
+      boot.initrd.luks.devices."luks-c05dea80-c718-46cf-85b8-9f3c6aeca86c".device = "/dev/disk/by-uuid/c05dea80-c718-46cf-85b8-9f3c6aeca86c";
+
+      swapDevices = [{device = "/dev/disk/by-uuid/bef52767-f7fc-405f-9423-d7b576e79c86";}];
 
       # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
       # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -138,6 +137,19 @@
       boot.loader.grub.enable = true;
       boot.loader.grub.device = "/dev/sda";
       boot.loader.grub.useOSProber = true;
+
+      # Setup keyfile
+      boot.initrd.secrets = {
+        "/crypto_keyfile.bin" = null;
+      };
+
+      # Enable grub cryptodisk
+      boot.loader.grub.enableCryptodisk = true;
+
+      boot.initrd.luks.devices."luks-c05dea80-c718-46cf-85b8-9f3c6aeca86c".keyFile = "/crypto_keyfile.bin";
+      # Enable swap on luks
+      boot.initrd.luks.devices."luks-90dc3598-b566-4723-8c36-ba6110d2bd98".device = "/dev/disk/by-uuid/90dc3598-b566-4723-8c36-ba6110d2bd98";
+      boot.initrd.luks.devices."luks-90dc3598-b566-4723-8c36-ba6110d2bd98".keyFile = "/crypto_keyfile.bin";
 
       # networking = {
       #   wireguard.interfaces =
@@ -175,7 +187,7 @@
       # this value at the release version of the first install of this system.
       # Before changing this value read the documentation for this option
       # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-      system.stateVersion = "24.11"; # Did you read the comment?
+      system.stateVersion = "22.05"; # Did you read the comment?
 
       services.x2goserver.enable = true;
 
