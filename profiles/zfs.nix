3065eac9a8f73a9c33975f36bbe03d7ad6d610d1
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2025-05-27 20:45:28 -0500

    fix: zfs

--- profiles/zfs.nix
+++ profiles/zfs.nix
@@ -1,5 +1,14 @@
-{
-  boot.zfs.enabled = true;
+{config, ...}: {
+  boot = {
+    zfs = {
+      forceImportAll = false;
+      forceImportRoot = false;
+    };
+    supportedFilesystems = {
+      zfs = true;
+    };
+  };
+  networking.hostId = builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName);
   services.zfs = {
     trim.enable = true;
     autoScrub.enable = true;
