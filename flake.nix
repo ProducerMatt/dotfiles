bdf570d46efdb58d3f434ab307ed1e103ac01dfd
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2025-07-18 12:46:51 -0500

    feat: don't overwrite nix in devshell

--- flake.nix
+++ flake.nix
@@ -225,7 +225,7 @@
         devShells.default = pkgs.mkShell {
           packages = with pkgs;
             [
-              nix-detsys
+              # nix-detsys only needed when bootstrapping a new sys
               colmena
               fish
               nixd
