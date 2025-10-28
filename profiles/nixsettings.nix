1650fbafa922806d641b123542c6864e0257d9dc
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2025-10-27 23:12:51 -0500

    feat: detsys use eval-cores

--- profiles/nixsettings.nix
+++ profiles/nixsettings.nix
@@ -38,6 +38,7 @@
       #   "cache.PherigoNAS.local-1:an8uYbjcJQKUvSdBEe/hlAbbHGDFH+sZZK6PpAQlSn8="
       # ];
       lazy-trees = true; # Determinate Systems Nix required
+      eval-cores = 0;
     };
     registry = let
       # NOTE: determinate already sets this
