69d0425fbc9f994889372409b7ea526d4c0a24a2
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2025-11-15 23:43:02 -0600

    feat: extra guidance for earlyOOM

--- profiles/earlyOOM.nix
+++ profiles/earlyOOM.nix
@@ -3,5 +3,30 @@
   # OOM-killer kicks in. But this can take a _long_ time. Let's get aggressive.
   services.earlyoom = {
     enable = true;
+    enableNotifications = true;
+    extraArgs = let
+      catPatterns = patterns: builtins.concatStringsSep "|" patterns;
+      # preferPatterns = [
+      # ".firefox-wrappe"
+      # "minetest"
+      # "vaultwarden"
+      # "java" # If it's written in java it's uninmportant enough it's ok to kill it
+      # ];
+      avoidPatterns = [
+        "bash"
+        "mosh-server"
+        "sshd"
+        "systemd"
+        "systemd-logind"
+        "systemd-udevd"
+        "tmux: client"
+        "tmux: server"
+        "nix"
+      ];
+    in [
+      # "--prefer" "'^(${catPatterns preferPatterns})$'"
+      "--avoid"
+      "'^(${catPatterns avoidPatterns})$'"
+    ];
   };
 }
